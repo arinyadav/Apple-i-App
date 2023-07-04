1c296e9c7bdb21fbdbdd9d2ae7af381c45077e0f
arin12345git config  --global user.name arin12345git config  --global user.name arin12345 <arinyadav1908@gmail.com>
2023-07-04 10:33:58 +0530

    final

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,95 +8,101 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool changedButton = false;
+
+  final _formkey = GlobalKey<FormState>();
+  moveToHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        changedButton = true;
+      });
+      await Future.delayed(const Duration(seconds: 1));
+      // ignore: use_build_context_synchronously
+      await Navigator.pushNamed(
+          context,
+          MyRoutes
+              .homeRoute); //from one screen to other push aage jao and pop peeche jao
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        // app bar replace material by skafold
+        // app bar can be replace material by skafold
         color: Colors.white, // background color
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login_page.png",
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Welcome $name",
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/login_page.png",
+                  fit: BoxFit.cover,
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: "Enter username",
-                        labelText: "Username",
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Welcome $name",
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Enter username",
+                          labelText: "Username",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username can't be empty";
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          name = value;
+                          // only for statefull widget
+                          setState(() {});
+                        },
                       ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Username can't be empty";
-                        }
-                        return null;
-                      },
-                      onChanged: (value) {
-                        name = value;
-                        // only for statefull widget
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true, // hiding password
-                      decoration: const InputDecoration(
-                        hintText: "Enter password",
-                        labelText: "Password",
+                      TextFormField(
+                        obscureText: true, // hiding password
+                        decoration: const InputDecoration(
+                          hintText: "Enter password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password can't be empty";
+                          } else if (value.length < 6) {
+                            return "Password length should be greater than 6 digits";
+                          }
+                          return null;
+                        },
                       ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Password can't be empty";
-                        } else if (value.length < 6) {
-                          return "Password length should be greater than 6 digits";
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const Text(
-                "Login Here",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(
+                  height: 40.0,
                 ),
-              ),
-              const SizedBox(
-                height: 40.0,
-              ),
-              ElevatedButton(
-                //you can use container and add animation to it
+                ElevatedButton(
+                  //you can use container and add animation to it
 
-                onPressed: () async {
-                  
-                    await Future.delayed(const Duration(seconds: 1));
-                    // ignore: use_build_context_synchronously
-                    await Navigator.pushNamed(
-                        context, MyRoutes.homeRoute); //from one screen to other push aage jao and pop peeche jao
-      
-                },
-                child: Text("Login"),
-                style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
-              )
-            ],
+                  onPressed: () => moveToHome(context),
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                )
+              ],
+            ),
           ),
         ));
   }
