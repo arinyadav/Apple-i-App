3dbf21f72b62f3190bc8fa3efa1eee675b3fcff5
arin12345git config  --global user.name arin12345git config  --global user.name arin12345 <arinyadav1908@gmail.com>
2023-07-06 10:38:30 +0530

    drawer changes

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,21 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final imageUrl =
+        "https://i.pinimg.com/736x/89/90/48/899048ab0cc455154006fdb9676964b3.jpg";
     return Drawer(
-      child:ListView(
-        children:const [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            
-            child: UserAccountsDrawerHeader(
-            margin: EdgeInsets.zero,
-            accountName: Text("Arin") ,
-            accountEmail: Text("arin@gmail.com"),))
-        ],
-    ),
+      child: Container(
+        color: Color.fromARGB(255, 28, 181, 227),
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 28, 181, 227)
+                  ),
+                  accountName:  Text("Arin"),
+                  accountEmail: Text("arin@gmail.com"),
+                  currentAccountPicture:CircleAvatar(
+                    backgroundImage:NetworkImage(imageUrl) ),
+                )
+                ),
+                const ListTile(
+                  leading:Icon(CupertinoIcons.home,color: Colors.white,),
+                  title: Text(
+                    "Home",
+                    textScaleFactor: 1.3,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                 const ListTile(
+                  leading:Icon(CupertinoIcons.profile_circled,color: Colors.white,),
+                  title: Text(
+                    "Profile",
+                    textScaleFactor: 1.3,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                 const ListTile(
+                  leading:Icon(CupertinoIcons.mail,color: Colors.white,),
+                  title: Text(
+                    "Mail Me!",
+                    textScaleFactor: 1.3,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+          ],
+        ),
+      ),
     );
-
   }
 }
