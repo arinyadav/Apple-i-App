98a35a6f34114074079f047aa12588c338cc3cab
arin12345git config  --global user.name arin12345git config  --global user.name arin12345 <arinyadav1908@gmail.com>
2023-07-31 08:54:35 +0530

    day-11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,13 +44,38 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                    ),
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items[index],
-                  );
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:BorderRadius.circular(10)
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Text(item.name,style: const TextStyle(color: Colors.white),),
+
+                        ),
+                        footer: Text(
+                        item.price.toString(),
+                      ),
+                      child: Image.network(item.image,
+                      )
+                      
+                      )
+                      );
                 },
+                itemCount: CatalogModel.items.length,
               )
             : const Center(
                 child: CircularProgressIndicator(),
